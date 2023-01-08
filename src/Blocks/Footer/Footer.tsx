7adc3bbbcaa9f4910dda6e471715f4df70a28bf9
Konstantin Rawle <costa08@mail.ru>
2023-01-08 21:49:40 +0100

    socials

--- src/Blocks/Footer/Footer.tsx
+++ src/Blocks/Footer/Footer.tsx
@@ -1,5 +1,7 @@
+import { Socials } from '../Socials';
 import { StyledContainer } from '../../globalStyles';
 
+
 export const Footer = () => {
-  return <StyledContainer>Footer</StyledContainer>
+  return <StyledContainer><Socials /></StyledContainer>
 }
