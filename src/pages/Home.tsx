007aa3411d080ee25b846ce964b80467595802f6
Konstantin Rawle <costa08@mail.ru>
2023-01-21 22:35:53 +0100

    footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+import { Footer } from '../Blocks/Footer/Footer';
+
 export const HomePage = () => {
-  return (<div>Nadishalowe</div>);
+  return (
+    <div>
+      Nadishalowe
+      <Footer />
+    </div>
+  );
 }
